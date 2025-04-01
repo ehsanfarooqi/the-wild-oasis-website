4e6abae86caa77f2b88753b6a59cd9f4cb090804
Ehsanullah Farooqi <ehsan.farooqi12@gmail.com>
2025-04-01 16:55:24 +0200

    message

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -12,6 +12,7 @@ function Logo() {
         height="60"
         width="60"
         alt="The Wild Oasis logo"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
       />
       <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
