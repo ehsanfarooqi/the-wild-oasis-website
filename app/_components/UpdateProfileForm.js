f5d6adb8c559be156c76ff1958258db848b21470
Ehsanullah Farooqi <ehsan.farooqi12@gmail.com>
2025-04-08 13:08:44 +0200

    use server actions

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,16 +1,22 @@
 "use client";
 
-function UpdateProfileForm({ children }) {
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
+import { updateGuest } from "../_lib/accions";
+
+function UpdateProfileForm({ guest, children }) {
+  const { fullName, email, countryFlag, nationalID } = guest;
 
   return (
     <div>
-      <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+      <form
+        action={updateGuest}
+        className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+      >
         <div className="space-y-2">
           <label>Full name</label>
           <input
             disabled
+            defaultValue={fullName}
+            name="fullName"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
           />
         </div>
@@ -19,6 +25,8 @@ function UpdateProfileForm({ children }) {
           <label>Email address</label>
           <input
             disabled
+            defaultValue={email}
+            name="email"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
           />
         </div>
@@ -41,6 +49,7 @@ function UpdateProfileForm({ children }) {
           <label htmlFor="nationalID">National ID number</label>
           <input
             name="nationalID"
+            defaultValue={nationalID}
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
           />
         </div>
